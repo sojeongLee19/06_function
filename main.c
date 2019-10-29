3de718cd7f647ea8fa4943db97db7a2c2f3fab1e
sojeongLee19 <55444000+sojeongLee19@users.noreply.github.com>
2019-10-29 15:00:35 +0900

    06

--- main.c
+++ main.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-
- 
-
- void sub(void)
- {
- 	   int auto_cpunt = 0;
- }
  
  /* run this program using the console pauser or add your own getch, system("pause") or input loop */
  
- int main (void)
- {
- 	  int i;
- 	  for (i=0;i<3;i++)
-           sub();
-           
- 
-      return v0;
- 
- }
+void sub (void)
+{
+	    int auto_count = 0;
+	    static int static_count = 0;
+	    
+	    auto_count++;
+	    static_count++;
+	    
+	    printf("auto : %i\n", auto_count);
+	    printf("static : %i\n", static_count);
+
+
+}
+       
